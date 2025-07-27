48211d59be1af6eaeabd2b7af2e19b824761c26e
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2025-07-27 11:22:14 -0400

    refactor

--- Milestones/CS-360-Inventory_Mobile_App/app/src/main/java/com/zybooks/inventoryapp/LoginActivity.java
+++ Milestones/CS-360-Inventory_Mobile_App/app/src/main/java/com/zybooks/inventoryapp/LoginActivity.java
@@ -5,20 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.zybooks.inventoryapp.helper.Helper;
 import com.zybooks.inventoryapp.model.UserDto;
 import com.zybooks.inventoryapp.model.ValidationResult;
 import com.zybooks.inventoryapp.repo.InventoryDatabase;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button btnRegister, btnLogin;
     private EditText edUserName,edPwd;
     private InventoryDatabase inventoryDatabase;
     @Override
@@ -30,29 +23,23 @@ public class LoginActivity extends AppCompatActivity {
         edUserName=findViewById(R.id.edUserName);
         edPwd = findViewById(R.id.edPwd);
 
-        btnRegister = findViewById(R.id.btnRegister);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create an intent to start SecondActivity
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);  // Start the second activity
-            }
+        Button btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(v -> {
+            // Create an intent to start SecondActivity
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);  // Start the second activity
         });
 
-        btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(v -> {
 
-                ValidationResult valid = IsValidToLogin();
-                if(!valid.hasError()){
-                    // Create an intent to start InventoryActivity
-                    Intent intent = new Intent(LoginActivity.this, InventoryActivity.class);
-                    startActivity(intent);  // Start the second activity
-                }else{
-                    Helper.SnackbarNotify(v,valid.getErrorMessage());
-                }
+            ValidationResult valid = IsValidToLogin();
+            if(!valid.hasError()){
+                // Create an intent to start InventoryActivity
+                Intent intent = new Intent(LoginActivity.this, InventoryActivity.class);
+                startActivity(intent);  // Start the second activity
+            }else{
+                Helper.SnackbarNotify(v,valid.getErrorMessage());
             }
         });
     }
@@ -66,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
             return vr;
         }
 
-        if(edPwd.getText().toString().length() == 0){
+        if(edPwd.getText().toString().isEmpty()){
             vr.setHasError(true);
             vr.setErrorMessage("Password can't be empty.");
             return vr;
--- Milestones/CS-360-Inventory_Mobile_App/app/src/main/java/com/zybooks/inventoryapp/model/UserDto.java
+++ Milestones/CS-360-Inventory_Mobile_App/app/src/main/java/com/zybooks/inventoryapp/model/UserDto.java
@@ -5,17 +5,17 @@ public class UserDto {
     private String username ="" ;
     private String password = "";
 
-    public UserDto(int id,String username, String passowrd){
+    public UserDto(int id,String username, String password){
         this.id= id;
         this.username= username;
-        this.password= passowrd;
+        this.password= password;
     }
     public UserDto(){
     }
 
-    public UserDto(String username, String passowrd){
+    public UserDto(String username, String password){
         this.username= username;
-        this.password= passowrd;
+        this.password= password;
     }
 
     // Getters
